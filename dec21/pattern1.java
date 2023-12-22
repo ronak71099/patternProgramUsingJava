87c161bc03ecf0d1433d3ad28e1e71af8640f178
ronak71099 <101537174+ronak71099@users.noreply.github.com>
2023-12-22 10:44:06 +0530

    Update pattern1.java
    *
    **
    ***
    ****
    ***** star pattern

--- dec21/pattern1.java
+++ dec21/pattern1.java
@@ -1,5 +1,9 @@
 package dec21;
-
+//star pattern                                                     *
+//                                                                 **
+//                                                                 ***
+//                                                                 ****
+//                                                                 *****
 public class pattern1 {
     public static void main(String[] args) {
         for(int i=0;i<5;i++){
